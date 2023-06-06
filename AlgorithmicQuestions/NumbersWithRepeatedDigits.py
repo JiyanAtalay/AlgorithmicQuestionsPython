061fa6ce127fe7068aa9c2a27850025337c99316
JiyanAtalay <135272170+JiyanAtalay@users.noreply.github.com>
2023-06-06 21:06:34 +0300

    Add files via upload

---
+++ AlgorithmicQuestions/NumbersWithRepeatedDigits.py
@@ -0,0 +1,30 @@
+def FindRepeatedDigits(num):
+
+    repeateds = 0
+
+    while True:
+        num = str(num)
+        length = len(num) -1
+        for i in range(0,length):
+            if num[i] == num[i+1]:
+                repeateds += 1
+            
+        num = int(num)
+        num -= 1
+        num = str(num)
+
+        if num == "0":break
+    
+    return repeateds
+
+
+while True:
+    flag = 1
+    try:
+        x = int(input("Sayi:"))
+    except ValueError:
+        print("Sayi girmeniz gerekmektedir\n")
+        flag = 0
+    if flag == 1:break
+
+print("Tekrar sayisi =",FindRepeatedDigits(x))
